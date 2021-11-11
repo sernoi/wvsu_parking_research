cef1a789648d3127380aeb19dc7e9bff96b6ab8e
sernoi <sernoi@DESKTOP-UFQGFLU>
2021-11-12 07:07:35 +0800

    start

---
+++ src/com/wvsu/park/Start.java
@@ -0,0 +1,16 @@
+package com.wvsu.park;
+
+import com.wvsu.park.controllers.MainFrameController;
+import com.wvsu.park.views.MainFrame;
+
+import javax.swing.*;
+
+public class Start {
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+       // mf.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        new MainFrameController(mf);
+        //camera starts recording...
+    }
+}
